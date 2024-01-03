6848876713455c4e716d1e5d58edda95486af9e2
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2024-01-03 11:29:10 -0500

    Adding responsive menu, and mobile responsive finished

--- client/src/components/header/NavBar.jsx
+++ client/src/components/header/NavBar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import menuOpen from "/icon-menu.svg";
+import menuClose from "/icon-menu-close.svg";
+import { useState } from "react";
 
 function NavBar() {
+  const [ isMenuOpen, setMenuOpen ] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className='hidden sm:flex'>
-        <ul className='flex sm:gap-8 text-neutral-darkGrayishBlue'>
+    <>
+    <div className='sm:flex'>
+    <button className={`me-4 sm:hidden ${isMenuOpen ? 'hidden' : 'block'}`} onClick={toggleMenu}>
+        <img src={menuOpen} alt="Open Menu" />
+      </button>
+        <ul className={`fixed top-0 right-0 h-full bg-black bg-opacity-50 w-full text-lg sm:space-y-0 sm:relative sm:flex sm:flex-row ${isMenuOpen ? 'block' : 'hidden'}`}>
+        <div className="bg-white text-neutral-veryDarkBlue font-medium h-full ps-6 py-6 space-y-5 ml-auto w-[260px] sm:w-auto sm:flex sm:gap-8 sm:text-neutral-darkGrayishBlue">
+        <button className={`ml-auto mb-16 me-4 sm:hidden ${isMenuOpen ? 'block' : 'hidden'}`} onClick={toggleMenu}>
+        <img src={menuClose} alt="Close Menu" />
+        </button>
             <li>
               <a href='#'>Home</a>
             </li>
@@ -19,8 +35,10 @@ function NavBar() {
             <li>
               <a href='#'>Categories</a>
             </li>
+        </div>
         </ul>
     </div>
+    </>
   )
 }
 
